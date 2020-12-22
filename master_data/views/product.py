e3c8e47c3c6abbb11033aac77142d7885416c0a5
sunil.thapa <sunil43thapa@gmail.com>
2020-12-22 14:02:26 +0545

    Completion of porduct for CRU operation with unit of measurement

--- master_data/views/product.py
+++ master_data/views/product.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
-from master_data.forms.product import ProdForm
+from master_data.forms.product import ProdForm, UomForm
 from master_data.models.product import *
 
 
@@ -25,3 +25,23 @@ class ProdUpdateView(UpdateView):
     form_class = ProdForm
     success_url = reverse_lazy('md-prod:list')
     context_object_name = 'prod'
+
+class UomCreateView(CreateView):
+    model = UnitOfMeasure
+    template_name = "master_data/uom/uom_create.html"
+    form_class = UomForm
+    success_url = reverse_lazy('md-prod:uom_list')
+
+
+class UomListView(ListView):
+    model = UnitOfMeasure
+    template_name = "master_data/uom/uom_list.html"
+    context_object_name = 'uoms'
+
+
+class UomUpdateView(UpdateView):
+    model = UnitOfMeasure
+    template_name = "master_data/uom/uom_update.html"
+    form_class = UomForm
+    success_url = reverse_lazy('md-prod:uom_list')
+    context_object_name = 'uom'
