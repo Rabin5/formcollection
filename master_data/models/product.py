5aa130421e6455eee8c61f6ab1e5a9c0552d224a
Narendra <narenpyakurel@gmail.com>
2021-01-17 17:19:33 +0545

    add_product

--- master_data/models/product.py
+++ master_data/models/product.py
@@ -13,12 +13,18 @@ class ProcurementMethod(models.Model):
         return self.name
 
 class Product(models.Model):
+    PRODUCT_CHOICES = (
+        ('Medicine', 'Medicine'),
+        ('Product', 'Product'),
+        ('Equipment','Equipment'),
+        ('ReliefMaterail','ReliefMaterial'),
+    )
     ordering = ['start_date']
     created = models.DateTimeField(auto_now_add=True)
     # date_end = models.DateTimeField(null=False, auto_now=True)
     name = models.CharField(max_length=255, blank=False, null=False, verbose_name='नाम')
     description = models.TextField(blank=True, verbose_name='वर्णन')
-    type = models.CharField(max_length=255, blank=False, null=False, verbose_name='प्रकार')
+    type = models.CharField(max_length=255, blank=False, null=False, verbose_name='प्रकार',choices=PRODUCT_CHOICES)
     uom = models.ForeignKey(UnitOfMeasure, on_delete=models.CASCADE, blank=True, null=True, verbose_name='मापनको एकाई')
 
 
--- static/js/post_data.js
+++ static/js/post_data.js
@@ -1,20 +1,24 @@
 function saveData(form,modal_id){
+    // window.location.reload();
     // var form = $(this);
     // console.log(form, form[0])
     var formdata = new FormData(form[0])
     formdata.append('body', '1');
     formdata.append('fiscal_year', '1');
-    if (action == ''){
-        action = form.attr('action')
-    }
+    action = form.attr('action');
+
     fetch(action, {
         method: form.attr("method"),
         body: formdata
     })
     .then(function(response){
-        response.text().then(function(data){
-            $(modal_id).find('.modal-content').html(data)
-        });
+        if (response.redirected == false) {
+            response.text().then(function(data){
+                $(modal_id).find('.modal-content').html(data)
+            });   
+        } else{
+            window.location.replace(response['url'])
+        }
     })
     .catch(function(err){
         console.log(err); 
