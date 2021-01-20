13b2a00c3921dd48fdd47eabd4d0252bdcc69b7f
Aadarsha Shrestha <aadarsha.shrestha@infodevelopers.com.np>
2021-01-20 16:17:00 +0545

    fix (migrations): remove all migrations and create a new migration that excludes creation of abstract class

--- forms/models/covid_hos_equip.py
+++ forms/models/covid_hos_equip.py
@@ -2,8 +2,7 @@ from django.db import models
 
 from forms.abstract import FormBaseModel, FormLineBaseModel
 from forms.utils import STATES
-from master_data.models import CovidHospital, Location, \
-    Product
+from master_data.models import CovidHospital, Product
 
 
 class CovidHospitalEquipment(FormBaseModel):
@@ -12,7 +11,6 @@ class CovidHospitalEquipment(FormBaseModel):
     """
 
     cov_hospital = models.ForeignKey(CovidHospital, on_delete=models.CASCADE, related_name='forms_cov_hos')
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="forms_location")
 
     def __str__(self):
         return f'{self.cov_hospital}'
