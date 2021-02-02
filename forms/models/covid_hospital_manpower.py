3a13b29b8dcb24e8a5851cb5ef5000d9420365f1
jenishacharya11 <jenish.acharya@infodevelopers.com.np>
2021-02-02 13:02:51 +0545

    str method updated for CovidHospital and CovidHospitalEquipment models

--- forms/models/covid_hospital_manpower.py
+++ forms/models/covid_hospital_manpower.py
@@ -12,7 +12,9 @@ class CovidHospitalManpower(FormBaseModel):
         CovidHospital, on_delete=models.PROTECT, verbose_name='कोभिड डेडिकेटेड अस्पातालको नाम:: ', null=True, blank=True)
 
     def __str__(self):
-        return self.covidhospital.name
+        if self.covidhospital:
+            return self.covidhospital.name
+        return ""
 
 
 class CovidHospitalManpowerLine(FormLineBaseModel):
