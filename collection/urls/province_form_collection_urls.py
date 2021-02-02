7cca1632fcd5f14d0ec1f5bf888647a41d20eec2
Jenish Acharya <jenish.acharya@infodevelopers.com.np>
2021-02-02 10:02:02 +0000

    Province Collection review page

--- collection/urls/province_form_collection_urls.py
+++ collection/urls/province_form_collection_urls.py
@@ -1,7 +1,9 @@
+from collection.views.province_form_collection_views import (
+    ProvinceFormCollectionCreateView, ProvinceFormCollectionDeleteView,
+    ProvinceFormCollectionListView, ProvinceFormCollectionReviewView,
+    ProvinceFormCollectionUpdateView, province_submit_form)
 from django.urls import path
 
-from collection.views.province_form_collection_views import ProvinceFormCollectionCreateView, ProvinceFormCollectionListView, \
-    ProvinceFormCollectionDeleteView, ProvinceFormCollectionUpdateView
 
 app_name = 'province_forms'
 urlpatterns = [
@@ -9,4 +11,6 @@ urlpatterns = [
     path('<int:pk>/update', ProvinceFormCollectionUpdateView.as_view(), name='update'),
     path('<int:pk>/delete', ProvinceFormCollectionDeleteView.as_view(), name='delete'),
     path('list', ProvinceFormCollectionListView.as_view(), name='list'),
+    path('<int:pk>/review', ProvinceFormCollectionReviewView.as_view(), name='review'),
+    path('<int:form_pk>/submit', province_submit_form, name='submit_form'),
 ]
